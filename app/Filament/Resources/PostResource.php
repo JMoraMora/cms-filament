1f562ce61b9331e39113a2db22661aeba3786c5b
Jaison Mora <jasonjairmoramora@gmail.com>
2024-05-11 17:46:05 -0500

    title y slug

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -7,11 +7,13 @@ use App\Filament\Resources\PostResource\RelationManagers;
 use App\Models\Post;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class PostResource extends Resource
 {
@@ -31,9 +33,12 @@ class PostResource extends Resource
                     ->required(),
                 Forms\Components\TextInput::make('title')
                     ->required()
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state)))
                     ->maxLength(255),
                 Forms\Components\TextInput::make('slug')
                     ->required()
+                    ->disabled()
                     ->maxLength(255),
                 Forms\Components\Textarea::make('body')
                     ->required()
